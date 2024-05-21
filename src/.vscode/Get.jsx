6d8ec3c48b0160c6105fb823bc6c21c842d9a46c
angelin <angelinsuguna05@gmail.com>
2024-05-21 16:26:47 +0530

    rest function added

--- src/.vscode/Get.jsx
+++ src/.vscode/Get.jsx
@@ -3,11 +3,11 @@ import { useState,useEffect } from 'react';
 
 
 const Get = () => {
-  const [breeds, setBreeds] = useState([]); 
+  const [show_data, set_data] = useState(); 
     useEffect(() => {
-        const apiUrl = 'https://dogapi.dog/api/v2/breeds';
+        const url = 'https://dogapi.dog/api/v2/breeds';
 
-        fetch(apiUrl) //promise
+        fetch(url) //promise
           .then(response => { //Handles info from fetch as promise
             if (!response.ok) { 
               throw new Error('Network response was not ok');
@@ -15,21 +15,18 @@ const Get = () => {
             return response.json();
           }) 
           .then(data => {
-            setBreeds(data.data);
-          })
+            set_data(data.data); //data1=response object from url 
+          })                      //data2=specific data property in response object
           .catch(error => {
-            console.error('Error:', error);
+            console.log('Error:', error);
           });
-      }, []);
+      },[]);
   return (
     <div>
-      <h1>Dog Breeds</h1>
-      <ul>
-        {breeds.map(breed => (
-          <li key={breed.id}>{breed.attributes.name}</li>
-        ))}
-        </ul>
-    </div>
+      <h1>FETCHING DATA</h1>
+     <ol> {show_data?(show_data.map((i)=>(<li><b>id:</b> {i.id}<br/><br/><b>Name: </b>{i.attributes.name}<br/><br/> {}</li>))):(<p>Cannot Display</p>)}</ol>
+     {/* {show_data?<p>{JSON.stringify(show_data)}</p>:<p>Cannot Display</p> } */}
+    </div>  
   );
 }
 
